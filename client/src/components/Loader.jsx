0bf618d04d7d90cf2ba8c3a4474f3a8b73973739
Cameron Faith <cmrnfaith@gmail.com>
2022-02-11 12:46:55 -0700

    Components complete

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,4 +1,7 @@
-const Loader = () => {
-  return <h1>Loader</h1>
-}
+const Loader = () => (
+  <div className="flex items-center justify-center py-3">
+    <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-red-700" />
+  </div>
+)
+
 export default Loader
