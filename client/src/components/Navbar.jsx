43dd6c33fdf0d73ea489b3a968708051204b4226
Cameron Faith <cmrnfaith@gmail.com>
2022-02-11 12:19:44 -0700

    Navbar base state

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,10 +3,24 @@ import { AiOutlineClose } from "react-icons/ai"
 
 import logo from "../../images/logo.png"
 
+const NavbarItem = ({ title, classProps }) => {
+  return <li className={"${classProps} mx-4 cursor-pointer"}>{title}</li>
+}
+
 const Navbar = () => {
   return (
     <nav className="justify-be items center flex w-full items-center justify-between p-4 md:justify-center">
-      <div className="md:flex-[0.5] flex-intial"
+      <div className="flex-intial items-center justify-center md:flex-[0.5]">
+        <img src={logo} alt={logo} className="w-32 cursor-pointer" />
+      </div>
+      <ul className="hidden flex-initial list-none flex-row items-center justify-between text-white md:flex">
+        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+          <NavbarItem key={item + index} title={item} />
+        ))}
+        <li className="mx-4 cursor-pointer rounded-full bg-[#2952e3] py-2 px-7 hover:bg-[#2546bd]">
+          Login
+        </li>
+      </ul>
     </nav>
   )
 }
